e929a705c0caedb77274ffa8db3fb793d7779d62
RafaelFS <rfl.fsilva@gmail.com>
2016-04-04 10:59:17 -0300

    Fixed model with modifications from professor

--- modelagem.js
+++ modelagem.js
@@ -1,12 +1,13 @@
 var lambda = 1;
-var mi = 1;
+var mic = 1;
+var mip = 1;
 var dt = 1;
 var C = 1;
 
-var A = math.matrix([[math.bignumber(1-3*lambda*dt*C), math.bignumber(mi*dt), 0, math.bignumber(mi*dt)],
-                     [math.bignumber(3*lambda*dt*C), math.bignumber(1-2*lambda*dt-mi*dt), 0, 0],
-                     [math.bignumber(3*lambda*dt*(1-C)), 0, math.bignumber(1-2*lambda*dt), 0],
-                     [0, math.bignumber(2*lambda*dt), math.bignumber(2*lambda*dt), math.bignumber(1-mi*dt)]]);
+var A = math.matrix([[math.bignumber(1-3*lambda*dt), math.bignumber(mic*dt), math.bignumber(mip*dt), math.bignumber(mic*dt)],
+                     [math.bignumber(3*lambda*dt*C), math.bignumber(1-2*lambda*dt-mic*dt), 0, 0],
+                     [math.bignumber(3*lambda*dt*(1-C)), 0, math.bignumber(1-2*lambda*dt-mip*dt), 0],
+                     [0, math.bignumber(2*lambda*dt), math.bignumber(2*lambda*dt), math.bignumber(1-mic*dt)]]);
 
 var p3Ok0 = math.bignumber(1);
 var p2Ok0 = math.bignumber(1);
